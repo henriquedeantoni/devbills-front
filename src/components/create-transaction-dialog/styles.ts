d1f22592408341abec0140dce354d8a2f3afb4c1
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-03-03 20:35:16 -0300

    Update styles.ts

--- src/components/create-transaction-dialog/styles.ts
+++ src/components/create-transaction-dialog/styles.ts
@@ -33,7 +33,7 @@ export const InputGroup = styled.div`
   flex-direction: column;
   gap: 0.75rem;
 
-  > label {
+  label {
     color: ${theme.colors.white};
     font-size: 0.625rem;
   }
@@ -47,11 +47,11 @@ export const InputGroup = styled.div`
     border: 1px solid transparent;
     transform: all 100ms;
 
-    $:focus {
+    &:focus {
       border-color: ${theme.colors.primary};
     }
   }
-`
+`;
 
 export const CurrencyInput = styled(InputNumberFormat)`
   height: 2.25rem;
@@ -65,11 +65,11 @@ export const CurrencyInput = styled(InputNumberFormat)`
   border: 1px solid transparent;
   transform: all 100ms;
 
-  $:focus {
+  &:focus {
     border-color: ${theme.colors.primary};
   }
 
-  $::placeholder {
+  &::placeholder {
     color: ${theme.colors.neutral};
   }
 `;
@@ -96,3 +96,4 @@ export const RadioGroup = styled.div`
     font-size: 0.875rem;
   }
 `;
+
